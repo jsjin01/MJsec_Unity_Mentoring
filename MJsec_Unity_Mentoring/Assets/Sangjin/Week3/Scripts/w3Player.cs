da6a84d1bd4bde2860f9e71ab6048ad0b4fa7958
Jeon sangjin <133385686+jsjin01@users.noreply.github.com>
2025-06-30 14:43:52 +0900

    week3 제작중

---
+++ MJsec_Unity_Mentoring/Assets/Sangjin/Week3/Scripts/w3Player.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class w3Player : MonoBehaviour
+{
+    [SerializeField]Rigidbody2D rb;
+    bool isGrounded = false;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector2.up * rb.gravityScale * 7, ForceMode2D.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("ground"))
+        {
+            isGrounded = true;
+        }
+    }
+}
